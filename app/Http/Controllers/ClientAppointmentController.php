a04cd69ecdac309f942d12d0a3ff159f077faf88
Jhoacin Escolástico Sánchez <130729052+Jhoacin3@users.noreply.github.com>
2023-10-18 16:08:38 -0600

    creacion de la estructura de appointment citas

---
+++ app/Http/Controllers/ClientAppointmentController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Client_Appointment;
+use App\Http\Requests\StoreClient_AppointmentRequest;
+use App\Http\Requests\UpdateClient_AppointmentRequest;
+
+class ClientAppointmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreClient_AppointmentRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Client_Appointment $client_Appointment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Client_Appointment $client_Appointment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateClient_AppointmentRequest $request, Client_Appointment $client_Appointment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Client_Appointment $client_Appointment)
+    {
+        //
+    }
+}
