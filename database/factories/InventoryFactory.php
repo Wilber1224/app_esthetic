d3623d9b6e236b1c50278afbbb2f88df348f226a
Jhoacin Escolástico Sánchez <130729052+Jhoacin3@users.noreply.github.com>
2023-10-18 16:11:35 -0600

    creacion de estructura vacia de inventory

--- database/factories/InventoryFactory.php
+++ database/factories/InventoryFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Appointment>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Inventory>
  */
-class AppointmentFactory extends Factory
+class InventoryFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -17,8 +17,7 @@ class AppointmentFactory extends Factory
     public function definition(): array
     {
         return [
-            'user_id' => 1,
-            'date' => now(),
+            //
         ];
     }
 }
